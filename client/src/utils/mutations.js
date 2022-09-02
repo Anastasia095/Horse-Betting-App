31dd81174e1258fcdadb2f23a23458989ad894fc
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-09-02 10:03:36 -0500

    login route working, can see profile page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
+  mutation addProfile($name: String!, $email: String!, $password: String!, $birthdate: Int!) {
+    addProfile(name: $name, email: $email, password: $password, birthdate: $birthdate) {
       token
       profile {
         _id
